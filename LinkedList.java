d21681dcca6baa010d475900f9ba18fd43e8a50a
MatthewOsei-Ansah <72076202+MatthewOsei-Ansah@users.noreply.github.com>
2022-12-03 12:05:45 +0000

    Update LinkedList.java

--- LinkedList.java
+++ LinkedList.java
@@ -42,10 +42,10 @@ public class LinkedList {   //The front of the priority queue, e.g the 1st eleme
 
             tail = new Element(value, current, null);// Create new tail Element pointing back to the previous Tail
             current.Next(tail);//Update the end of the LinkedList to point to this new Element
-            length ++;
+            length++;
         } else {//Add the front of the linked list
             front = new Element(value, null, null);
-            length ++;
+            length++;
         }
     }
 
@@ -55,71 +55,144 @@ public class LinkedList {   //The front of the priority queue, e.g the 1st eleme
         Element current;
         Element removed = null;
         current = front;
+        boolean found = false;
 
-        if (current.Next() != null) {
+        if (current != null) {
             while (current.Next() != null) {
                 if (current.Value().equals(value)) {
                     removed = current;
                     current.Previous().Next(current.Next());
                     current.Next().Previous(current.Previous());
-                    length --;
+                    length--;
+                    found = true;
                     return removed.Value();
+                } else {
+                    current = current.Next();
                 }
-                current = current.Next();
+            }if(current.Next() == null && !found){
+                throw new IllegalArgumentException();
             }
+        } else {
+            throw new IllegalArgumentException();
         }
-        else {
-            front = removed;
-            front = null;
-            length --;
-            return removed.Value();
-        }
-        throw new IllegalArgumentException();
+        return removed.Value();
     }
 
-    public String pop()
-    {//Remove the 1st element from the queue and reorganise the queue
+    public String pop() {//Remove the 1st element from the queue and reorganise the queue
         //Throw an error if the queue is empty
-        System.out.println(front.Value());
-        front = front.Next();
-        if(isEmpty()) {
+        Element popped;
+        if (!isEmpty()) {
+            popped = front;
+            front.Next().Previous(null);
+            front = front.Next();
+            length--;
+            return popped.Value();
+        } else if (isEmpty()) {
             throw new UnsupportedOperationException();
         }
+        return front.Value();
     }
 
-    public String pop(int index)
-    {//Remove from the queue the element at the specified index  reorganise the queue
+
+    public String pop(int index) {//Remove from the queue the element at the specified index  reorganise the queue
         //Throw an error if the index is invalid
-        throw new IndexOutOfBoundsException();
-    }
-
-    public void insert(String value, int index)
-    {//Insert the item into the correct position in the linked list
-        //Throw an error if the index is invalid
-        throw new IndexOutOfBoundsException();
-    }
-
-    public int index(String value)
-    {//Return the position of the first occurrence of the value in the linked list
-        //Throw an error if the value does not exist
-        throw new IllegalArgumentException();
-    }
-
-    public int length()
-    {//Return the number of elements in the Linked List
-        return -1;
-    }
-
-    public boolean search(String value)
-    {//Return true if the value exists in the Linked List
-        boolean found;
-        Element current = front;
-        while(current.Next() != null){
-            if(current.Value().equals(value)){
-
+        Element current;
+        Element popped = null;
+        int count = 0;
+        if (!isEmpty()) {
+            if (0 <= index && index < length) {
+                current = front;
+                while (count < index) {
+                    current = current.Next();
+                    count++;
+                }
+                popped = current;
+                current.Previous().Next(current.Next());
+                current.Next().Previous(current.Previous());
+                length--;
+                return popped.Value();
+            } else {
+                throw new IndexOutOfBoundsException();
             }
         }
-        return false;
+        return popped.Value();
     }
 
+    public void insert(String value, int index) {//Insert the item into the correct position in the linked list
+        //Throw an error if the index is invalid
+        Element current;
+        Element Insert;
+        Element FrontInsert;
+        int count = 0;
+        if (!isEmpty()) {
+            if (index > 0 && index < length) {
+                current = front;
+                while (count < index) {
+                    current = current.Next();
+                    count++;
+                }
+                Insert = new Element(value, current.Previous(), current);
+                current.Previous().Next(Insert);
+                current.Previous(Insert);
+                length++;
+            } else if (index == 0) {
+                FrontInsert = new Element(value, null, front);
+                front.Previous(FrontInsert);
+                front = FrontInsert;
+                length++;
+            } else {
+                throw new IndexOutOfBoundsException();
+            }
+        }else{
+            throw new IndexOutOfBoundsException();
+        }
+    }
+
+    public int index(String value) {//Return the position of the first occurrence of the value in the linked list
+        //Throw an error if the value does not exist
+        Element current;
+        int index = 0;
+        boolean found = false;
+        if (!isEmpty()) {
+            current = front;
+            while (current.Next() != null) {
+                if (current.Value().equals(value)) {
+                    found = true;
+                    return index;
+                } else {
+                    current = current.Next();
+                    index++;
+                }
+            } if(current.Next() == null && !found){
+                throw new IllegalArgumentException("Value is not in list");
+            }
+        } else {
+            throw new IllegalArgumentException("Value is not in list");
+        }
+        return index;
+    }
+
+    public int length() {//Return the number of elements in the Linked List
+        return length;
+    }
+
+    public boolean search(String value) {//Return true if the value exists in the Linked List
+        boolean found = false;
+        Element current = front;
+        if (!isEmpty()) {
+            while (current.Next() != null) {
+                if (current.Value().equals(value)) {
+                    found = true;
+                    break;
+                } else {
+                    current = current.Next();
+                }
+            }
+        }
+        if (!found) {
+            return false;
+        } else {
+            return true;
+        }
+    }
 }
