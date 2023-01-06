d13cfc06b6f0d0cb557e9c48e91753903eb392da
MatthewOsei-Ansah <72076202+MatthewOsei-Ansah@users.noreply.github.com>
2023-01-06 09:25:22 +0000

    Create Connector.java

---
+++ Connector.java
@@ -0,0 +1,29 @@
+package hashing;
+
+public class Connector {
+    private Integer Weight;
+    private String Node;
+
+    public Connector(String node, Integer weight){
+        this.Weight = weight;
+        this.Node = node;
+    }
+
+    public String getNode(){
+        return this.Node;
+    }
+
+    public Integer getWeight(){
+        return this.Weight;
+    }
+
+    public void setWeight(Integer weight){
+        this.Weight = weight;
+    }
+
+    public void setNode(String node){
+        this.Node = node;
+    }
+
+
+}
