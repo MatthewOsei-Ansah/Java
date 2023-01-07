63a3b4e2dcc10e0a4f58985dd14a1c48681ec0a1
MatthewOsei-Ansah <72076202+MatthewOsei-Ansah@users.noreply.github.com>
2023-01-07 14:38:54 +0000

    Update Graph.java

--- Graph.java
+++ Graph.java
@@ -1,64 +1,67 @@
-package hashing;
+import java.util.ArrayList;
 
-
-
-public class Hashing implements IHash {
+public class Graph implements IHash {
     private final int MAX_SIZE = 7;
     private Node[] Array = {null,null,null,null,null,null,null};
     private int length = 0;
-    public String[] asArray()
-    {
-        int i = 0;
-        String[] StringArray = {null,null,null,null,null,null,null};
-        for(Node value: Array){
-            if(value == null){
-                StringArray[i] = null;
-                i++;
+    public void DisplayConnections (){
+        String Step1;
+        String Step2;
+        String Step3;
+        for(Node node: Array){
+            ArrayList<String> StringArray = new ArrayList<>();
+            if(node == null){
+                continue;
             }else{
-                StringArray[i] = value.getNode();
-                i++;
+                for(Connector connector: node.returnConnections()){
+                    Step1 = connector.getNode();
+                    Step2 = connector.getWeight().toString();
+                    Step3 = Step1 + " : " + Step2;
+                    StringArray.add(Step3);
+                    }
+                System.out.println(node.getNode() + " : " + StringArray);
+                }
             }
         }
-        return StringArray;
-    }    
+
 
     @Override
-    public void add(int Key, String Value) {
+    public void add(String Value) {
         boolean Placed = false;
         int i = 1;
         Integer Address;
-        Node node = new Node(Value, Key);
+        Node node = new Node(Value);
         node.AddressGenerator(MAX_SIZE);
         Address = node.getAddress();
-            for(Node CurrentNode : Array){
-                if(CurrentNode == null){
-                    continue;
-                }else if(CurrentNode.getKey() == node.getKey()){
-                    throw new IllegalArgumentException("A value with that key already exists within the dictionary");
-                }
+        for(Node CurrentNode : Array){
+            if(CurrentNode == null){
+                continue;
+            }else if(CurrentNode.getKey() == node.getKey()){
+                throw new IllegalArgumentException("A value with that key already exists within the dictionary");
             }
-            for(Node CurrentNode2 : Array){
-                if(CurrentNode2 == null){
-                    continue;
-                }else if(CurrentNode2.getNode().equals(node.getNode())){
-                    throw new IllegalArgumentException("Node with this name already exists");
-                }
+        }
+        for(Node CurrentNode2 : Array){
+            if(CurrentNode2 == null){
+                continue;
+            }else if(CurrentNode2.getNode().equals(node.getNode())){
+                throw new IllegalArgumentException("Node with this name already exists");
             }
+        }
 
-            if(Array[Address] == null){
-                Array[Address] = node;
-                length++;
-            }else {
-                while (!Placed && i != MAX_SIZE - 1) {
-                    if (Array[(Address + i) % MAX_SIZE] == null) {
-                        Array[(Address + i) % MAX_SIZE] = node;
-                        node.setAddress(i, MAX_SIZE);
-                        Placed = true;
-                        length++;
-                    } else {
-                        i++;
-                    }
+        if(Array[Address] == null){
+            Array[Address] = node;
+            length++;
+        }else {
+            while (!Placed && i != MAX_SIZE - 1) {
+                if (Array[(Address + i) % MAX_SIZE] == null) {
+                    Array[(Address + i) % MAX_SIZE] = node;
+                    node.setAddress(i, MAX_SIZE);
+                    Placed = true;
+                    length++;
+                } else {
+                    i++;
                 }
+            }
             if(!Placed){
                 throw new UnsupportedOperationException("Dictionary Full");
             }
@@ -72,24 +75,60 @@ public class Hashing implements IHash {
             if(CurrentNode == null){
                 continue;
             }else if(CurrentNode.getNode().equals(node)){
-                CurrentNode.addConnection(addnode, weight);
                 found = true;
-                for(Node Currentnode2 : Array){
-                    if(Currentnode2 == null){
-                        continue;
-                    }else if(Currentnode2.getNode().equals(addnode)){
-                        Currentnode2.addConnection(node, weight);
-                        found2 = true;
-                    }
-                }
             }
         }
+
         if(!found){
             throw new IllegalArgumentException("A node with that name was not found");
         }
+
+        for(Node Currentnode2 : Array){
+            if(Currentnode2 == null){
+                continue;
+            }else if(Currentnode2.getNode().equals(addnode)){
+                found2 = true;
+            }
+        }
+
         if(!found2){
             throw new IllegalArgumentException("Can not add connection, node does not exist");
         }
+
+        for(Node CurrentNode : Array){
+            if(CurrentNode == null){
+                continue;
+            }else if(CurrentNode.getNode().equals(node)){
+                CurrentNode.addConnection(addnode, weight);
+            }
+        }
+
+        for(Node Currentnode2 : Array){
+            if(Currentnode2 == null){
+                continue;
+            }else if(Currentnode2.getNode().equals(addnode)){
+                Currentnode2.addConnection(node, weight);
+            }
+        }
+    }
+
+    public Integer getKey(String node){
+        boolean found = false;
+        Integer keyvalue = null;
+
+        for(Node Node : Array){
+            if(Node == null){
+                continue;
+            } else if (Node.getNode().equals(node)) {
+                found = true;
+                keyvalue = Node.getKey();
+                return keyvalue;
+            }
+        }
+        if(!found){
+            throw new IllegalArgumentException("Node with name given does not exist");
+        }
+        return keyvalue;
     }
 
     @Override
@@ -124,8 +163,8 @@ public class Hashing implements IHash {
                     length--;
                 }
             }
-    }else {
-        throw new IllegalArgumentException("Dictionary is empty");
+        }else {
+            throw new IllegalArgumentException("Dictionary is empty");
         }
         if(!found){
             throw new IllegalArgumentException("Value with the inputted key was not found");
