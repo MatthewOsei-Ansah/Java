1285cda9e1c698d88b2d33ddae75711c3176cfb6
MatthewOsei-Ansah <72076202+MatthewOsei-Ansah@users.noreply.github.com>
2023-01-07 14:41:02 +0000

    Update IHash.java

--- IHash.java
+++ IHash.java
@@ -1,12 +1,10 @@
-package hashing;
-
 import java.lang.String;
 
 public interface IHash {
-    void add(int Key, String Value);
+    void add(String Value);
     void delete(int Key);
     String item(int Key);
     boolean contains(int Key);
     int length();
-    boolean isEmpty();    
+    boolean isEmpty();
 }
