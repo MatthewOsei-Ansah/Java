7f63e65dec92e82e0b7766c3603e42c1b7234de9
MatthewOsei-Ansah <72076202+MatthewOsei-Ansah@users.noreply.github.com>
2023-01-10 12:53:11 +0000

    Create GenericHashingELement.java

---
+++ GenericHashingELement.java
@@ -0,0 +1,26 @@
+package com.company;
+
+public class Element<T, K> {
+    K Key;
+    T Value;
+    private String StringValue;
+
+    public Element(T value, K key){
+        this.Value = value;
+        this.Key =  key;
+    }
+
+    public T getValue(){
+        return this.Value;
+    }
+
+    public K getKey(){
+        return this.Key;
+    }
+
+    @Override
+    public String toString(){
+        StringValue = this.Value.toString();
+        return StringValue;
+    }
+}
