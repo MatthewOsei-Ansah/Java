08fc2ba865b735bf29e4299e5942f286ce88bc61
MatthewOsei-Ansah <72076202+MatthewOsei-Ansah@users.noreply.github.com>
2023-01-13 15:33:32 +0000

    Create BinaryTree.java

---
+++ BinaryTree.java
@@ -0,0 +1,16 @@
+public class BinaryTree<T> {
+    private Node<T> Root;
+    private int NumofNodes = 0;
+
+    public void addnode(T value){
+        Node<T> current;
+        int compare;
+        Node<T> newValue = new Node<>(value, )
+        
+        if(Root != null){
+            current = Root;
+            compare = Root.compareTo();
+            if()
+        }
+    }
+}
