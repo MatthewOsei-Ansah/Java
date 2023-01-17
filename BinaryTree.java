e8836973ba94d15af3ebb1be912e1acbdc5f9cf9
MatthewOsei-Ansah <72076202+MatthewOsei-Ansah@users.noreply.github.com>
2023-01-17 08:49:13 +0000

    Update BinaryTree.java

--- BinaryTree.java
+++ BinaryTree.java
@@ -1,16 +1,71 @@
+import java.util.ArrayList;
+
 public class BinaryTree<T> {
     private Node<T> Root;
-    private int NumofNodes = 0;
+    private Node<T> Current;
 
     public void addnode(T value){
-        Node<T> current;
         int compare;
-        Node<T> newValue = new Node<>(value, )
-        
+        Node<T> newValue = new Node<>(value, null, null);
+
         if(Root != null){
-            current = Root;
-            compare = Root.compareTo();
-            if()
+            compare = Current.compareTo(newValue);
+            if(compare < 0){
+                if(Current.getLeft() == null){
+                    Current.setLeft(Current, newValue);
+                    Current = Root;
+                }else{
+                    Current = Current.getLeft();
+                    addnode(value);
+                }
+            } else if (compare > 0) {
+                if(Current.getRight() == null){
+                    Current.setRight(Current ,newValue);
+                    Current = Root;
+                }else{
+                    Current = Current.getRight();
+                    addnode(value);
+                }
+            }else{
+                throw new IllegalArgumentException("A node with that name already exists within the tree");
+            }
+        }
+        if(Root == null){
+            Root = new Node<>(value, null, null);
+            Current = Root;
         }
     }
+
+
+    public void preOrderTraversal(Node<T> root) {
+        if(root == null) {
+            return;
+        }
+        System.out.print(root.getValue() + " ");
+        preOrderTraversal(root.getLeft());
+        preOrderTraversal(root.getRight());
+    }
+
+    public void inOrderTraversal(Node<T> root) {
+        if(root == null) {
+            return;
+        }
+        inOrderTraversal(root.getLeft());
+        System.out.print(root.getValue() + " ");
+        inOrderTraversal(root.getRight());
+    }
+
+    public void postOrderTraversal(Node<T> root) {
+        if(root == null) {
+            return;
+        }
+        postOrderTraversal(root.getLeft());
+        postOrderTraversal(root.getRight());
+        System.out.print(root.getValue() + " ");
+    }
+
+    public Node<T> getRoot(){
+        return Root;
+    }
 }
+
