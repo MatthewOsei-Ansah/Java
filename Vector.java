adf613473f109416e449a34307f3e5ce35b66423
MatthewOsei-Ansah <72076202+MatthewOsei-Ansah@users.noreply.github.com>
2023-01-22 17:42:21 +0000

    Create Vector.java

---
+++ Vector.java
@@ -0,0 +1,45 @@
+public class Vector implements IVector
+{
+    double X;
+    double Y;
+    public Vector(double x, double y)
+    {
+        this.X = x;
+        this.Y = y;
+    }
+
+
+    public IVector add(IVector a, IVector b){
+        return new Vector(a.getx() + b.getx(),a.gety() + b.gety());
+    }
+
+
+    public IVector multiply(double scalar, IVector a){
+        return new Vector(scalar * a.getx(),scalar * a.gety());
+    }
+
+
+    public int dotProduct(IVector a, IVector b){
+        return (int) ((a.getx() * b.getx()) + (a.gety() * b.gety()));
+    }
+
+
+    public IVector convexCombination(double scalara, IVector a, double scalarb, IVector b){
+        return new Vector((a.getx() * scalara) + (b.getx() * scalarb),(a.gety() * scalara) + (b.gety() * scalarb));
+    }
+
+    public double getx() {
+        return X;
+    }
+    
+
+    public double gety() {
+        return Y;
+    }    
+    
+    @Override
+    public String toString()
+    {
+        return Double.toString(getx()) + ":" + Double.toString(gety());
+    }
+}
